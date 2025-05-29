91ec29995900d3daca4c0a5e20d5f1c2aba8a826
front42 <altaifront@gmail.com>
2025-05-29 13:09:59 +0300

    feat: add visual flip effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,10 @@ import { RouterOutlet, RouterLink, Router, NavigationEnd } from '@angular/router
 import { DecimalPipe, NgIf } from '@angular/common';
 import { filter } from 'rxjs';
 
+class Album {
+  constructor(public front: string, public back: string, public isFlipped: boolean) {}
+}
+
 @Component({
   standalone: true,
   selector: 'app-root',
@@ -13,24 +17,36 @@ import { filter } from 'rxjs';
 export class AppComponent implements OnInit {
   protected isMainPage: boolean = true;
   protected isPlaying: boolean = false;
-  protected albumsInOrder: string[] = [];
-  protected albums: string[] = [];
+  protected albumsInOrder: Album[] = [];
+  protected albums: Album[] = [];
   protected minutes: number = 0;
   protected seconds: number = 0;
   protected intervalId: any;
 
   constructor(private router: Router) {
     for (let i = 0; i < 14; i++) {
-      this.albumsInOrder.push(`assets/album-${i < 10 ? '0' + i : i}.jpg`, `assets/album-${i < 10 ? '0' + i : i}.jpg`);
+      this.albumsInOrder.push(
+        new Album(`url(assets/album-${i < 10 ? '0' + i : i}.jpg)`, 'url(assets/vinyl-lp.png)', true),
+        new Album(`url(assets/album-${i < 10 ? '0' + i : i}.jpg)`, 'url(assets/vinyl-lp.png)', true)
+      );
     }
     this.albums = this.albumsInOrder.slice();
+  }
+
+  protected flipAlbum(index: number, event: MouseEvent): void {
+    const album = event.currentTarget as HTMLDivElement;
+    album.style.zIndex = '1';
+    setTimeout(() => {
+      album.style.zIndex = '0';
+    }, 1000);
+    this.albums[index].isFlipped = !this.albums[index].isFlipped;
   }
 
   protected order() {
     this.albums = this.albumsInOrder.slice();
   }
 
-  protected shuffle(array: string[]): string[] {
+  protected shuffle(array: Album[]): Album[] {
     const shuffledArr = array.slice();
     for (let i = shuffledArr.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
@@ -42,6 +58,7 @@ export class AppComponent implements OnInit {
   protected play(): void {
     this.isPlaying = true;
     this.albums = this.shuffle(this.albumsInOrder);
+    setTimeout(() => this.albums.forEach((album) => (album.isFlipped = false)), 5000);
     this.intervalId = setInterval(() => {
       this.seconds += 1;
       if (this.seconds === 60) {
@@ -55,7 +72,11 @@ export class AppComponent implements OnInit {
     this.isPlaying = false;
     this.minutes = this.seconds = 0;
     clearInterval(this.intervalId);
-    this.order();
+    setTimeout(() => this.albums.forEach((album) => (album.isFlipped = false)));
+    setTimeout(() => {
+      this.order();
+      this.albums.forEach((album) => (album.isFlipped = true));
+    }, 1000);
   }
 
   ngOnInit(): void {
