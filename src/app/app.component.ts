bf9f17f4f6d057cb578902460a52c2f0e5e30a77
front42 <altaifront@gmail.com>
2025-06-07 19:20:46 +0300

    feat: implement game basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,11 +17,15 @@ class Album {
 export class AppComponent implements OnInit {
   protected isMainPage: boolean = true;
   protected isPlaying: boolean = false;
+  protected isPregap: boolean = false;
   protected albumsInOrder: Album[] = [];
   protected albums: Album[] = [];
   protected minutes: number = 0;
   protected seconds: number = 0;
   protected intervalId: any;
+  protected firstFlipped: Album | null = null;
+  protected activeAnimation: boolean = false;
+  protected matchCounter: number = 0;
 
   constructor(private router: Router) {
     for (let i = 0; i < 14; i++) {
@@ -30,53 +34,55 @@ export class AppComponent implements OnInit {
         new Album(`url(assets/album-${i < 10 ? '0' + i : i}.jpg)`, 'url(assets/vinyl-lp.png)', true)
       );
     }
-    this.albums = this.albumsInOrder.slice();
+    this.order();
   }
 
-  protected flipAlbum(index: number, event: MouseEvent): void {
-    const album = event.currentTarget as HTMLDivElement;
-    album.style.zIndex = '1';
-    setTimeout(() => {
-      album.style.zIndex = '0';
-    }, 1000);
-    this.albums[index].isFlipped = !this.albums[index].isFlipped;
-  }
-
-  protected order() {
+  protected order(): void {
     this.albums = this.albumsInOrder.slice();
   }
 
   protected shuffle(array: Album[]): Album[] {
-    const shuffledArr = array.slice();
-    for (let i = shuffledArr.length - 1; i > 0; i--) {
+    const shuffledArray = array.slice();
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
-      [shuffledArr[i], shuffledArr[j]] = [shuffledArr[j], shuffledArr[i]];
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
     }
-    return shuffledArr;
+    return shuffledArray;
   }
 
   protected play(): void {
+    if (this.minutes || this.seconds) return;
+    this.seconds = 10;
+    this.isPregap = true;
     this.isPlaying = true;
     this.albums = this.shuffle(this.albumsInOrder);
-    setTimeout(() => this.albums.forEach((album) => (album.isFlipped = false)), 5000);
-    this.intervalId = setInterval(() => {
-      this.seconds += 1;
-      if (this.seconds === 60) {
-        this.seconds = 0;
-        this.minutes += 1;
-      }
-    }, 1000);
+    this.intervalId = setInterval(() => (this.seconds -= 1), 1000);
+    setTimeout(() => {
+      clearInterval(this.intervalId);
+      this.seconds = 0;
+      this.isPregap = false;
+      this.albums.forEach((album) => (album.isFlipped = false));
+      this.intervalId = setInterval(() => {
+        this.seconds += 1;
+        if (this.seconds === 60) {
+          this.seconds = 0;
+          this.minutes += 1;
+        }
+      }, 1000);
+    }, 10000);
   }
 
   protected stop(): void {
-    this.isPlaying = false;
-    this.minutes = this.seconds = 0;
     clearInterval(this.intervalId);
-    setTimeout(() => this.albums.forEach((album) => (album.isFlipped = false)));
+    this.matchCounter = 0;
+    this.isPlaying = false;
+    this.firstFlipped = null;
+    this.albums.forEach((album) => (album.isFlipped = false)); // for what earlier there was needed setTimeout with 0
     setTimeout(() => {
       this.order();
       this.albums.forEach((album) => (album.isFlipped = true));
-    }, 1000);
+    }, 900);
+    setTimeout(() => (this.minutes = this.seconds = 0), 1700);
   }
 
   protected toggleFullscreen(): void {
@@ -91,6 +97,47 @@ export class AppComponent implements OnInit {
     if (event.code === 'F11') {
       event.preventDefault();
       this.toggleFullscreen();
+    }
+  }
+
+  protected flipAlbum(album: Album): void {
+    if (this.activeAnimation) return;
+    this.activeAnimation = true;
+    album.isFlipped = true;
+    if (!this.firstFlipped) {
+      this.activeAnimation = false;
+      this.firstFlipped = album;
+    } else if (this.firstFlipped.front === album.front) {
+      this.matchCounter++;
+      if (this.matchCounter === 14) {
+        clearInterval(this.intervalId);
+        setTimeout(() => {
+          alert(
+            `worKING alert:\nYou did it in ${
+              this.minutes ? (this.minutes !== 1 ? this.minutes + ' minutes' : this.minutes + ' minute') : ''
+            }${this.seconds ? (this.seconds !== 1 ? ' ' + this.seconds + ' seconds' : ' ' + this.minutes + ' second') : ''}!`
+          );
+          this.stop();
+        }, 1200);
+      }
+      this.activeAnimation = false;
+      this.firstFlipped = null;
+    } else {
+      setTimeout(() => {
+        this.firstFlipped!.isFlipped = album.isFlipped = false;
+        this.activeAnimation = false;
+        this.firstFlipped = null;
+      }, 1200);
+    }
+  }
+
+  protected adjustZIndex(event: TransitionEvent): void {
+    if (event.propertyName === 'transform') {
+      const albumElement = event.currentTarget as HTMLDivElement;
+      albumElement.style.zIndex = '1';
+      setTimeout(() => {
+        albumElement.style.zIndex = '0';
+      }, 900);
     }
   }
 
